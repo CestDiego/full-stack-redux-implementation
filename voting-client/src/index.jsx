82c17e17cc9c11737faa758d6b40e29bb6af6fd5
Diego Berrocal <cestdiego@gmail.com>
2015-10-11 17:29:56 -0400

    Action creators!  ftw

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import Voting from './components/Voting';
+import ReactDOM from 'react-dom';
+import Router, {Route} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 
-const pair = ['Trainspotting', '28 Days Later'];
+import reducer from './reducer';
+import App from './components/App';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
 
-React.render(
-  <Voting pair={pair} winner='Trainspotting' />,
+const store = createStore(reducer);
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+
+socket.on('state', state =>
+  store.dispatch({type: 'SET_STATE', state})
+);
+
+const routes = (
+  <Route component={App}>
+    <Route path="/results" component={ResultsContainer}/>
+    <Route path="/" component={VotingContainer} />
+  </Route>
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 );
